598e8492ec97172cc8de1a21b56abf2f58db41f1
Valmira Shala <valmirashalaa@gmail.com>
2022-04-09 15:31:46 +0200

    created socket

---
+++ clientside.js
@@ -0,0 +1,12 @@
+var PORT = 33333;
+var HOST = '192.168.31.192';
+
+var dgram = require('dgram');
+var message = 'My KungFu is Good!';
+
+var client = dgram.createSocket('udp4');
+client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+  if (err) throw err;
+  console.log('UDP message sent to ' + HOST +':'+ PORT);
+  client.close();
+});

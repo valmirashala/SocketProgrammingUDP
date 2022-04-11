975e3125052a654d892f776718bdd9c693613ff8
vanesaaliu <94717763+vanesaaliu@users.noreply.github.com>
2022-04-11 15:36:43 +0200

    deleted unnecessary code

--- permissions.js
+++
@@ -1,9 +0,0 @@
-import { actions } from "./constants.js";
-
-const mappings = new Map();
-
-mappings.set(actions.MODIFY_FILE, [roles.ADMIN]);
-mappings.set(actions.VIEW_FILE, [roles.ADMIN, roles.GUEST]);
-mappings.set(actions.EXECUTE_FILE, [roles.ADMIN]);
-mappings.set(actions.CREATE_FILE, [roles.ADMIN]);
-
